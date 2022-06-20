073b7b16d46bf712d1eeb80933aac44751a8bff0
septovan-ovan <672015027@student.uksw.edu>
2022-06-21 04:13:42 +0700

    Use implicit intent to open camera

--- app/src/main/java/com/example/inputpointfti/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/inputpointfti/ui/home/HomeFragment.java
@@ -1,20 +1,45 @@
 package com.example.inputpointfti.ui.home;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.inputpointfti.databinding.FragmentHomeBinding;
 
+import static android.app.Activity.RESULT_OK;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private ImageView imgView;
+
+    ActivityResultLauncher<Intent> getPhoto = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            (result) -> {
+                if (result.getResultCode() == RESULT_OK)
+                {
+                    Bundle extras = result.getData().getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    imgView.setImageBitmap(imageBitmap);
+                }
+            });
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +51,15 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        final ImageButton btnAddPhoto = binding.btnAddPhoto;
+        btnAddPhoto.setOnClickListener((view) -> {
+            Intent intentOpenCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            getPhoto.launch(intentOpenCamera);
+        });
+
+        imgView = binding.imgView;
+
         return root;
     }
 
