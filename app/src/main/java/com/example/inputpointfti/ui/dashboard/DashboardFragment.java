e56c16a1280f6d9c6f5aa8c5f31043348889407b
BoflyEta <bofly321@gmail.com>
2022-06-21 17:00:53 +0700

    fragment

--- app/src/main/java/com/example/inputpointfti/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/inputpointfti/ui/dashboard/DashboardFragment.java
@@ -4,30 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.inputpointfti.R;
 import com.example.inputpointfti.databinding.FragmentDashboardBinding;
+import com.example.inputpointfti.ui.notifications.NotificationsViewModel;
+
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        DashboardViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
+
+
+
+
 
     @Override
     public void onDestroyView() {
--- app/src/main/java/com/example/inputpointfti/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/inputpointfti/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.inputpointfti.ui.home;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
