09c7551ebddd52f73a10bd244c3d54902b3c333b
septovan-ovan <672015027@student.uksw.edu>
2022-07-05 21:47:26 +0700

    Implemented interacting with camera using implicit intent

--- app/src/main/java/com/example/inputpointfti/ui/dashboard/LihatPoinFragment.java
+++ app/src/main/java/com/example/inputpointfti/ui/dashboard/LihatPoinFragment.java
@@ -1,66 +1,72 @@
 package com.example.inputpointfti.ui.dashboard;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.inputpointfti.R;
+import com.example.inputpointfti.databinding.FragmentLihatPoinBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LihatPoinFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import static android.app.Activity.RESULT_OK;
+
 public class LihatPoinFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentLihatPoinBinding binding;
+    private ImageView imgResult;
 
     public LihatPoinFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LihatPoinFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LihatPoinFragment newInstance(String param1, String param2) {
-        LihatPoinFragment fragment = new LihatPoinFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    ActivityResultLauncher<Intent> getPhoto = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK){
+                        Bundle extras = result.getData().getExtras();
+                        Bitmap imageCapturedBitmap = (Bitmap) extras.get("data");
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+                        imgResult.setImageBitmap(imageCapturedBitmap);
+                    }
+                }
+            }
+        );
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lihat_poin, container, false);
+
+        binding = FragmentLihatPoinBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        ImageButton btnAddPhoto = binding.btnAddPhoto;
+        btnAddPhoto.setOnClickListener((view) -> {
+            Intent intentOpenCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            getPhoto.launch(intentOpenCamera);
+        });
+
+        imgResult = binding.imgResult;
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
