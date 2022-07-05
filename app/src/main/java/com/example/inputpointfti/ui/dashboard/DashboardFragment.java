ee85e1d53e0f820e18c560c917204a350621f58d
BoflyEta <bofly321@gmail.com>
2022-07-05 17:57:16 +0700

    fragment

--- app/src/main/java/com/example/inputpointfti/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/inputpointfti/ui/dashboard/DashboardFragment.java
@@ -6,32 +6,57 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.inputpointfti.R;
-import com.example.inputpointfti.databinding.FragmentDashboardBinding;
-import com.example.inputpointfti.ui.notifications.NotificationsViewModel;
+import com.google.android.material.tabs.TabLayout;
 
 
 public class DashboardFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    public DashboardFragment() {
+        // Required empty public constructor
     }
 
-
-
-
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        tabLayout = view.findViewById(R.id.tabmenu);
+        viewPager = view.findViewById(R.id.viewpager);
+        return view;
+    }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        tabLayout.addTab(tabLayout.newTab().setText("Lihat Poin"));
+        tabLayout.addTab(tabLayout.newTab().setText("Input Poin"));
+        PagerAdapter pagerAdapter = new PagerAdapter(getContext(), getChildFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setOffscreenPageLimit(pagerAdapter.getCount() > 1 ? pagerAdapter.getCount() - 1 : 1);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
